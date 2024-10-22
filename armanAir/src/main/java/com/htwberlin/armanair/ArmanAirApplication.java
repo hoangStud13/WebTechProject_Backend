86fff95cf36b9bdfb577adb9c35dd016197de30c
Dung <s0592847@htw-berlin.de>
2024-10-22 18:38:44 +0200

    Test

---
+++ armanAir/src/main/java/com/htwberlin/armanair/ArmanAirApplication.java
@@ -0,0 +1,13 @@
+package com.htwberlin.armanair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmanAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmanAirApplication.class, args);
+	}
+
+}
