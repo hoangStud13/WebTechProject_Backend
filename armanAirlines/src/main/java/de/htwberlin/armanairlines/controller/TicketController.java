0dd966a93fb19b30e5d5d4a9cf1bb3ad05b4c5de
Hoang <hoang1303x@gmail.com>
2024-10-23 08:51:31 +0200

    Added Response and ErrorResponse in Controller

--- armanAirlines/src/main/java/de/htwberlin/armanairlines/controller/TicketController.java
+++ armanAirlines/src/main/java/de/htwberlin/armanairlines/controller/TicketController.java
@@ -1,10 +1,14 @@
 package de.htwberlin.armanairlines.controller;
 
 
+import de.htwberlin.armanairlines.constants.TicketConstants;
+import de.htwberlin.armanairlines.dto.ResponseDto;
 import de.htwberlin.armanairlines.dto.TicketDto;
 import de.htwberlin.armanairlines.entities.Ticket;
 import de.htwberlin.armanairlines.service.ITicketService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,23 +23,46 @@ public class TicketController {
     }
 
     @GetMapping("/tickets/{flightId}")
-    public TicketDto getTicket(@PathVariable Long flightId){
+    public ResponseEntity<TicketDto> getTicket(@PathVariable Long flightId){
         TicketDto ticketDto = ticketService.readTicket(flightId);
-        return ticketDto;
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ticketDto);
     }
 
     @PostMapping("/ticket")
-    public void addTicket(@RequestBody TicketDto ticketDto){
+    public ResponseEntity<ResponseDto> addTicket(@RequestBody TicketDto ticketDto){
         ticketService.createTicket(ticketDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(TicketConstants.MESSAGE_201,TicketConstants.STATUS_201));
     }
 
     @PutMapping("/ticket")
-    public void updateTicket(@RequestBody TicketDto ticketDto){
-        ticketService.updateTicket(ticketDto);
+    public ResponseEntity<ResponseDto> updateTicket(@RequestBody TicketDto ticketDto){
+        boolean isUpdated = ticketService.updateTicket(ticketDto);
+        if(isUpdated){
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(TicketConstants.MESSAGE_200,TicketConstants.STATUS_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(TicketConstants.MESSAGE_417_UPDATE,TicketConstants.STATUS_417));
+        }
     }
 
     @DeleteMapping("/ticket/{flightId}")
-    public void deleteTicket(@PathVariable Long flightId){
-        ticketService.deleteTicket(flightId);
+    public ResponseEntity<ResponseDto> deleteTicket(@PathVariable Long flightId){
+        boolean isDeleted = ticketService.deleteTicket(flightId);
+        if(isDeleted){
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(TicketConstants.MESSAGE_200,TicketConstants.STATUS_200));
+        }else{
+            return  ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(TicketConstants.MESSAGE_417_DELETE,TicketConstants.STATUS_417));
+        }
     }
 }
