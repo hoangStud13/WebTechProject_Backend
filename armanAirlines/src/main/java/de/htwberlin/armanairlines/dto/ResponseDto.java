316079da80763bb5bed082422e764f2980723e6c
Hoang <hoang1303x@gmail.com>
2024-10-23 08:22:41 +0200

    Added Response and ErrorResponseDto

---
+++ armanAirlines/src/main/java/de/htwberlin/armanairlines/dto/ResponseDto.java
@@ -0,0 +1,13 @@
+package de.htwberlin.armanairlines.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDto {
+    private String statusMessage;
+    private String statusCode;
+}
